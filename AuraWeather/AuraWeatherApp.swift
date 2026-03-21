59f34dc0e68f910a66a7fb367f4c4c0302951150
rashid-404-cmd <abdulrashidzsh@code.edu.az>
2026-03-21 23:38:08 +0400

    Initial Commit

---
+++ AuraWeather/AuraWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraWeatherApp.swift
+//  AuraWeather
+//
+//  Created by Rashid Shalbuzov on 21.03.26.
+//
+
+import SwiftUI
+
+@main
+struct AuraWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuraWeather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AuraWeather
+//
+//  Created by Rashid Shalbuzov on 21.03.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
